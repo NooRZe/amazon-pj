013b3dfebfbd3d0200c3d971e5f079e113186517
NooRZe <noorze9@gmail.com>
2025-01-29 21:47:27 +0300

    сделал кнопку add интерактивной

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,13 +45,38 @@ products.forEach((product) => {    //продукты грузятся из от
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" 
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
   `;
 }); 
 
-console.log(productsHTML);
+
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart') //выбирает все кнопки Add в массиве
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+     const productId = button.dataset.productId; //dataset получает все данные записанные в тэге после data-name, в JS все "-" заменяются было product-name стало productName
+     
+     let matchingItem; //задаем переменную для проверки есть ли в корзине повторяющийся элемент 
+     cart.forEach((item) => {   //перебирает каждый объект в массиве корзины
+      if (productId === item.productId) { //сравниваем имя выбраного кликом объекта с находящимися в массиве корзины объектами 
+        matchingItem = item; //если имена совпадают, они попадают в переменную 
+      }
+     });
+
+     if (matchingItem) {  //если имя совпадает то в его объекте просто добавляется +1 к количеству
+      matchingItem.quantity +=1;
+     } else {              //если не совпадает - то в корзине нет таких вещей и создается новый (пушится) объект массива
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+        console.log(cart);
+       });
+     });
