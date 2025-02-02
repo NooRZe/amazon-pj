14b5751979b043f158521ff420a5d2e0fa8df66b
NooRZe <noorze9@gmail.com>
2025-02-02 15:12:57 +0300

    локалка лоя корзины

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,18 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];  //export позволяет использовать код, не записывая новый тег скрипт
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];  //export позволяет использовать код, не записывая новый тег скрипт
+}
+ 
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart (productId) {
   let matchingItem; //задаем переменную для проверки есть ли в корзине повторяющийся элемент 
@@ -24,6 +30,7 @@ export function addToCart (productId) {
       quantity: 1
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart (productId) {
@@ -34,4 +41,5 @@ export function removeFromCart (productId) {
     } 
   });
   cart = newCart;
+  saveToStorage();
 }
