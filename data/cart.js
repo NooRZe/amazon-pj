1a8b48059ecb806d7bb8fc074128a6a02ee4fc9e
NooRZe <noorze9@gmail.com>
2025-02-02 14:22:16 +0300

    чекаут

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,12 @@
-export const cart = [];  //export позволяет использовать код, не записывая новый тег скрипт
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];  //export позволяет использовать код, не записывая новый тег скрипт
+
+
 
 export function addToCart (productId) {
   let matchingItem; //задаем переменную для проверки есть ли в корзине повторяющийся элемент 
