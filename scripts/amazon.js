f938a8b6a2a987fcf7ddb22b84b6a8c6fc7ee848
NooRZe <noorze9@gmail.com>
2025-02-02 13:36:23 +0300

    перенос функций в отдельный файл импорт в качестве модуля

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
-import { cart } from '../data/cart.js'; //.. переходит на папку выше, импорт переменной карт из отдельного файла , as ** позволяет переназначить имя импортированного 
+import { cart, addToCart } from '../data/cart.js'; //.. переходит на папку выше, импорт переменной карт из отдельного файла , as ** позволяет переназначить имя импортированного 
 import { products } from '../data/products.js';
+
 let productsHTML = ''; //сохраняем результаты форича в переменную
 products.forEach((product) => {    //продукты грузятся из отдельного файла
   productsHTML = productsHTML + `
@@ -58,33 +59,19 @@ products.forEach((product) => {    //продукты грузятся из от
 
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
+function updateCartQuantity () {
+  let cartQuantity = 0;
+      cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
 document.querySelectorAll('.js-add-to-cart') //выбирает все кнопки Add в массиве
   .forEach((button) => {
     button.addEventListener('click', () => {
-     const productId = button.dataset.productId; //dataset получает все данные записанные в тэге после data-name, в JS все "-" заменяются было product-name стало productName
-     
-     let matchingItem; //задаем переменную для проверки есть ли в корзине повторяющийся элемент 
-     cart.forEach((item) => {   //перебирает каждый объект в массиве корзины
-      if (productId === item.productId) { //сравниваем имя выбраного кликом объекта с находящимися в массиве корзины объектами 
-        matchingItem = item; //если имена совпадают, они попадают в переменную 
-      }
+      const productId = button.dataset.productId; //dataset получает все данные записанные в тэге после data-name, в JS все "-" заменяются было product-name стало productName
+    addToCart(productId);
+    updateCartQuantity();
      });
-
-     if (matchingItem) {  //если имя совпадает то в его объекте просто добавляется +1 к количеству
-      matchingItem.quantity +=1;
-     } else {              //если не совпадает - то в корзине нет таких вещей и создается новый (пушится) объект массива
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-          cartQuantity += item.quantity;
-        });
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-      });
-     });
+  });
 
